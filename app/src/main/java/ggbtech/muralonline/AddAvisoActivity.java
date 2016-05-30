cdba9ecd0e4866fa5075718d7600c995f65b46b3
Leonardo Afonso <nardoasafonso@gmail.com>
2016-05-30 11:40:45 -0300

    Menu Adicionado na tela AddAviso

--- app/src/main/java/ggbtech/muralonline/AddAvisoActivity.java
+++ app/src/main/java/ggbtech/muralonline/AddAvisoActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -54,6 +56,34 @@ public class AddAvisoActivity extends AppCompatActivity {
     @Override
     public void onBackPressed(){
         startActivity(new Intent(this,MainActivity.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_add) {
+            Intent intent = new Intent(this, AddAvisoActivity.class);
+            startActivity(intent);
+            return true;
+        }else if (id == R.id.action_list) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
