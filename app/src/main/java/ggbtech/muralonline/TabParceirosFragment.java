290a52f74f5f8727ebbe58867e99aa971be614a9
nardoafonso <nardoasafonso@gmail.com>
2016-10-14 14:35:51 -0300

    ImageRequest dos parceiros implementad (falta so  deixar organizado para mais parceiros)

--- app/src/main/java/ggbtech/muralonline/TabParceirosFragment.java
+++ app/src/main/java/ggbtech/muralonline/TabParceirosFragment.java
@@ -1,15 +1,27 @@
 package ggbtech.muralonline;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.Volley;
 
 public class TabParceirosFragment extends Fragment {
-
+    private ImageView mImageView;
+    private RequestQueue rq;
+    private Context myContext;
+    private TextView mTextView;
 
     public TabParceirosFragment() {
         // Required empty public constructor
@@ -23,7 +35,31 @@ public class TabParceirosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_parceiros, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_tab_parceiros, container, false);
+        myContext = getContext();
+        String url= "https://a2ua.com/imagem/imagem-006.jpg";
+        mImageView = (ImageView)rootView.findViewById(R.id.imgview);
+        mTextView = (TextView)rootView.findViewById(R.id.txtParceiros);
+
+        ImageRequest request = new ImageRequest(
+                url,
+                new Response.Listener<Bitmap>() {
+            @Override
+            public void onResponse(Bitmap response) {
+                    mTextView.setText("Imagem Baixada");
+                    mImageView.setImageBitmap(response);
+            }
+        },0 , 0, null, Bitmap.Config.RGB_565,
+                new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                    mTextView.setText("Erro ao baixar imagem");
+            }
+        });
+        rq = Volley.newRequestQueue(myContext);
+        rq.add(request);
+        return rootView;
     }
 }
